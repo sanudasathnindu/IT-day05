c750772ce734639282a1c3bea61a14cb43dbab87
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-07 16:53:48 +0530

    commit19

--- js/app.js
+++ js/app.js
@@ -118,6 +118,18 @@
 
 //for loop
 
-for (let i = 0; i < 10; i++) {
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+//--------------------------------------------------
+
+//while loop
+
+let i = 0;
+
+while (i < 10) {
     console.log(i);
+    i++;
 }
