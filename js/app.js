3072a436038239052660d9bbfe01e192af16013e
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-07 19:42:16 +0530

    commit23

--- js/app.js
+++ js/app.js
@@ -163,7 +163,13 @@
 
 //Arrays
 
-let numbers = [1, 2, 3, 4, 5];
+// let numbers = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+let numbers = [1, "saman", true, 4, 5];
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
