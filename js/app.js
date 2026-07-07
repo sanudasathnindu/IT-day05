c40cda8d1b09bb77864a6a31c879795e06a4db0a
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-07 16:29:01 +0530

    commit12

--- js/app.js
+++ js/app.js
@@ -10,16 +10,27 @@
 //  alert("Hello ");
 
 
-let name = "Saman";
-let age = 18;
-let isAfterAl = false;
-let customer = [];
-let customer1 = {};
-let salary = 7500.50;
+// let name = "Saman";
+// let age = 18;
+// let isAfterAl = false;
+// let customer = [];
+// let customer1 = {};
+// let salary = 7500.50;
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isAfterAl);
-console.log(typeof customer);
-console.log(typeof customer1);
-console.log(typeof salary);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAfterAl);
+// console.log(typeof customer);
+// console.log(typeof customer1);
+// console.log(typeof salary);
+
+//--------------------------------------------------
+
+// + | - | * | /
+
+let x = 10;
+let y = 20;
+
+let z = x + y;
+
+console.log(z);
