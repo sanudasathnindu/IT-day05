6f6d25bdd1f6279b56fdf967b58cd6df84e6eb57
sanudasathnindu <sanudasathnindu5@gmail.com>
2026-07-07 16:51:59 +0530

    commit18

--- js/app.js
+++ js/app.js
@@ -86,30 +86,38 @@
 
 //Switch Statements
 
-let day = 2;
+// let day = 2;
 
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Invalid day");
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+//--------------------------------------------------
+
+//for loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
 }
